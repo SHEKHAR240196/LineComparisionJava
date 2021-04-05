9cfb16fd94058f9be596d15e012034c9fc458a49
shekhar <shekharpatle999@gmail.com>
2021-04-05 16:15:35 +0530

    added: lenth of line

--- line.java
+++ line.java
@@ -1,9 +1,17 @@
-package LinCom;
+public class Calculating_Length {
 
-public class wlcmToLineComp {
-	public static void main(String[] args) {
-	
-		System.out.println("Welcome to line Comparision");
-
-}
+	public static void main(String[] args){
+		int x1;
+		int x2;
+		int y1;
+		int y2;
+		
+		x1=30;
+		x2=60;
+		y1=50;
+		y2=70;
+		
+		double len=Math.sqrt((x2-x1)^2+(y2-y1)^2);
+		System.out.println("the length of line between two cordinates"+"("+x1+","+x2+")"+"("+y1+","+y2+")"+"is:->"+len);
+	}
 }
