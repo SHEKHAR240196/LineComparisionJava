4ce3bf6eb54a520930af0a017c0ad593d3027076
shekhar <shekharpatle999@gmail.com>
2021-04-05 16:21:24 +0530

    added: LineComparision

--- line.java
+++ line.java
@@ -1,36 +1,30 @@
-public class EquityOfLines {
-	public static void main(String[] args) {
-	
-	int x1;
-	int x2;
-	int y1;
-	int y2;
-	int j1;
-	int j2;
-	int k1;
-	int k2;
-	
-	x1=30;
-	x2=60;
-	y1=50;
-	y2=70;
-	j1=80;
-	j2=90;
-	k1=30;
-	k2=40;
+public class LineComparision {
+	public static void main(String arg[]){
+		int x1,x2,y1,y2,j1,j2,k1,k2;
+		x1=20;
+		x2=40;
+		y1=40;
+		y2=80;
+		j1=10;
+		j2=20;
+		k1=20;
+		k2=40;
 
-	double A = Math.sqrt((x2-x1)^2+(y2-y1)^2);
-	double B = Math.sqrt((j2-j1)^2+(k2-k1)^2);
-	System.out.println("the length of line one"+"("+x1+","+x2+")"+"("+y1+","+y2+")"+"is:->"+ A);
-	System.out.println("the length of line two"+"("+x1+","+x2+")"+"("+y1+","+y2+")"+"is:->"+ B);
-	String s1 = A+"";
-	String s2 = B+""; 
-	if(s1.equals(s2) == true)
-		System.out.println("the two lines are equal");
-	else
-		System.out.println("the two lines are not equal");
+		double A=Math.sqrt((x2-x1)^2+(y2-y1)^2);
+		double B=Math.sqrt((j2-j1)^2+(k2-k1)^2);
+		System.out.println("the length of line one"+"("+x1+","+x2+")"+"("+y1+","+y2+")"+"is:->"+A);
+		System.out.println("the length of line two"+"("+x1+","+x2+")"+"("+y1+","+y2+")"+"is:->"+B);
+		String s1 = A+"";
+		String s2 = B+"";
+		double res = s1.compareTo(s2);
+		if(res == 0)
+			System.out.println("lines are equal");
+		else if(res < 0.0)
+			System.out.println("line one is smaller thean line two");
+		else  
+			System.out.println("lines one is greater then line two");
+			
 }
-
 
 
 }
